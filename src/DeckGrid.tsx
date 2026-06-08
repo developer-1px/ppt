b6971c08e60eeb5c6884d9cf1eed0bdaf26078ba
유용태 <teo.v@navercorp.com>
2026-06-08 17:34:35 +0900

    refactor(styles): 덱 그리드 스타일 분리

--- src/DeckGrid.tsx
+++ src/DeckGrid.tsx
@@ -7,6 +7,7 @@ import {
   htmlSlideRootAttributes,
 } from './htmlSlideContract'
 import { cssVariables } from './cssVariables'
+import './DeckGrid.css'
 
 type DeckGridProps = {
   activeSlideId: string
